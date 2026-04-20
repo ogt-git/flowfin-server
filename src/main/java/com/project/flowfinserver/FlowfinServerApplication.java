32b12a1229bd64f690bb8c105614511d40a00521
ogt-git <rudxor4@gmail.com>
2026-04-20 13:20:45 +0900

    create : flowfin 프로젝트 생성

---
+++ src/main/java/com/project/flowfinserver/FlowfinServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.flowfinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowfinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowfinServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/flowfinserver/FlowfinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.flowfinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowfinServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

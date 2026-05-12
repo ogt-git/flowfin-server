40320e699017b34fd48dabce07eb16c0af2b8de1
ogt-git <rudxor4@gmail.com>
2026-05-12 12:27:54 +0900

    [chore] 빌드 설정 및 앱 진입점 업데이트

--- src/main/java/com/project/flowfinserver/FlowfinServerApplication.java
+++ src/main/java/com/project/flowfinserver/FlowfinServerApplication.java
@@ -2,8 +2,10 @@ package com.project.flowfinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FlowfinServerApplication {
 
     public static void main(String[] args) {

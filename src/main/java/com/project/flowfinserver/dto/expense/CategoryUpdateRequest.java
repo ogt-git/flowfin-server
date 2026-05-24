b50ae3a7fedd81dbeb08e8b0c4f7cc2cadd3c458
ogt-git <rudxor4@gmail.com>
2026-05-25 05:34:39 +0900

    fix : 날짜 형식 수정(YYYYMM)

--- src/main/java/com/project/flowfinserver/dto/expense/CategoryUpdateRequest.java
+++ src/main/java/com/project/flowfinserver/dto/expense/CategoryUpdateRequest.java
@@ -3,8 +3,10 @@ package com.project.flowfinserver.dto.expense;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class CategoryUpdateRequest {
 
--- src/main/java/com/project/flowfinserver/repository/ExpenseStatsRepository.java
+++ src/main/java/com/project/flowfinserver/repository/ExpenseStatsRepository.java
@@ -34,7 +34,7 @@ public interface ExpenseStatsRepository extends Repository<Expense, Long> {
            "FROM Expense e " +
            "WHERE e.userId = :userId " +
            "AND e.isExcluded = false " +
-           "AND FUNCTION('DATE_FORMAT', e.expenseDate, '%Y-%m') = :month")
+           "AND FUNCTION('DATE_FORMAT', e.expenseDate, '%Y%m') = :month")
     Long findTotalAmountByUserIdAndMonth(
             @Param("userId") Long userId,
             @Param("month") String month);

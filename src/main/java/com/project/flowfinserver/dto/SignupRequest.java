87117474ec6c99749c19da19ec6dd4fea157d9de
ogt-git <rudxor4@gmail.com>
2026-05-27 22:18:53 +0900

    add : 회원 및 인증 유효성 검사 추가

--- src/main/java/com/project/flowfinserver/dto/SignupRequest.java
+++ src/main/java/com/project/flowfinserver/dto/SignupRequest.java
@@ -1,12 +1,33 @@
 package com.project.flowfinserver.dto;
 
 import com.project.flowfinserver.domain.RiskType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class SignupRequest {
+
+    @NotBlank(message = "이메일은 필수입니다")
+    @Email(message = "이메일 형식이 올바르지 않습니다")
+    @Size(max = 100, message = "이메일은 100자 이내여야 합니다")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다")
+    @Size(min = 8, max = 20, message = "비밀번호는 8~20자여야 합니다")
+    @Pattern(
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).+$",
+        message = "비밀번호는 영문, 숫자, 특수문자를 각 1개 이상 포함해야 합니다"
+    )
     private String password;
+
+    @NotBlank(message = "이름은 필수입니다")
+    @Size(max = 30, message = "이름은 30자 이내여야 합니다")
     private String name;
+
+    @NotNull(message = "투자 성향은 필수입니다")
     private RiskType riskType;
 }
--- src/main/java/com/project/flowfinserver/dto/user/UpdateProfileRequest.java
+++ src/main/java/com/project/flowfinserver/dto/user/UpdateProfileRequest.java
@@ -1,14 +1,20 @@
 package com.project.flowfinserver.dto.user;
 
 import com.project.flowfinserver.domain.RiskType;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UpdateProfileRequest {
+
+    @Size(max = 30, message = "이름은 30자 이내여야 합니다")
     private String name;
+
     private RiskType riskType;
     private String currentPassword;
+
+    @Size(min = 8, max = 20, message = "비밀번호는 8~20자여야 합니다")
     private String newPassword;
 }

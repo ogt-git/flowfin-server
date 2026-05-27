f34ca44c80d2a7448bee77ac1721a6d0807660f0
ogt-git <rudxor4@gmail.com>
2026-05-27 19:51:08 +0900

    fix : codef 연동 복수계정 제외

--- src/main/java/com/project/flowfinserver/dto/codef/CodefStockRequest.java
+++ src/main/java/com/project/flowfinserver/dto/codef/CodefStockRequest.java
@@ -18,9 +18,4 @@ public class CodefStockRequest {
     private String account;          // 계좌번호 - 숫자만 (필수)
 
     private String accountPassword;  // 계좌비밀번호 (평문 입력 → 서버에서 RSA 암호화)
-    private String inquiryType;      // 조회구분 "0":결제기준(기본) "1":계좌비번검증 "2":체결기준
-
-    // 키움증권 복수 아이디 보유 고객용
-    private String id;               // 복수계정 아이디
-    private String addPassword;      // 복수계정 패스워드 (평문 입력 → 서버에서 RSA 암호화)
 }

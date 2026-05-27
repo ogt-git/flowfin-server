8448fa74c2931a7be7539a2fb3b6940e8fd2a98f
ogt-git <rudxor4@gmail.com>
2026-05-27 22:21:10 +0900

    add : 커뮤니티 및 지출 유효성 검사 추가

--- src/main/java/com/project/flowfinserver/dto/CommentRequest.java
+++ src/main/java/com/project/flowfinserver/dto/CommentRequest.java
@@ -1,9 +1,15 @@
 package com.project.flowfinserver.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CommentRequest {
+
+    @NotBlank(message = "댓글 내용은 필수입니다")
+    @Size(max = 1000, message = "댓글은 1000자 이내여야 합니다")
     private String content;
+
     private boolean anonymous;
 }

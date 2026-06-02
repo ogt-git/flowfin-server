29c97fe319784e0b2374d40a7737630638d4f4e1
ogt-git <rudxor4@gmail.com>
2026-06-02 12:26:23 +0900

    fix : codef 오류 및 보안 설정 수정

--- src/main/java/com/project/flowfinserver/codef/CodefApiClient.java
+++ src/main/java/com/project/flowfinserver/codef/CodefApiClient.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -33,33 +34,50 @@ public class CodefApiClient {
 
     // 계정 연결 (connectedId 발급)
     public String createAccount(HashMap<String, Object> accountMap) throws Exception {
-        log.info("[CODEF] createAccount params={}", accountMap);
+        log.info("[CODEF] createAccount org={}", resolveOrganization(accountMap));
         String response = easyCodef.createAccount(resolveServiceType(), accountMap);
-        log.info("[CODEF] createAccount response={}", response);
+        log.debug("[CODEF] createAccount completed");
         return response;
     }
 
     // 계정 연결 해지
     public String deleteAccount(HashMap<String, Object> accountMap) throws Exception {
-        log.info("[CODEF] deleteAccount params={}", accountMap);
+        log.info("[CODEF] deleteAccount org={}", resolveOrganization(accountMap));
         String response = easyCodef.deleteAccount(resolveServiceType(), accountMap);
-        log.info("[CODEF] deleteAccount response={}", response);
+        log.debug("[CODEF] deleteAccount completed");
         return response;
     }
 
     // 계정 정보 업데이트 (인증 재시도용)
     public String updateAccount(HashMap<String, Object> accountMap) throws Exception {
-        log.info("[CODEF] updateAccount params={}", accountMap);
+        log.info("[CODEF] updateAccount org={}", resolveOrganization(accountMap));
         String response = easyCodef.updateAccount(resolveServiceType(), accountMap);
-        log.info("[CODEF] updateAccount response={}", response);
+        log.debug("[CODEF] updateAccount completed");
         return response;
     }
 
     // 데이터 조회
     public String requestProduct(String productUrl, HashMap<String, Object> params) throws Exception {
-        log.info("[CODEF] url={} params={}", productUrl, params);
+        log.info("[CODEF] requestProduct url={}", productUrl);
         String response = easyCodef.requestProduct(productUrl, resolveServiceType(), params);
-        log.info("[CODEF] response={}", response);
+        log.debug("[CODEF] requestProduct completed url={}", productUrl);
         return response;
+    }
+
+    private Object resolveOrganization(HashMap<String, Object> parameters) {
+        Object organization = parameters.get("organization");
+        if (organization != null) {
+            return organization;
+        }
+
+        Object accountList = parameters.get("accountList");
+        if (accountList instanceof Iterable<?> accounts) {
+            for (Object account : accounts) {
+                if (account instanceof Map<?, ?> accountMap) {
+                    return accountMap.get("organization");
+                }
+            }
+        }
+        return null;
     }
 }
